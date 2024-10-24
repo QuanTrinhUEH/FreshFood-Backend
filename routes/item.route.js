b9f9a07dfdfd3a446c807495d16d07477e61a90d
Quan <143281625+QuanTrinhUEH@users.noreply.github.com>
2024-10-24 11:32:59 +0700

    Fix item

--- routes/item.route.js
+++ routes/item.route.js
@@ -7,7 +7,7 @@ const itemRouter = Router();
 
 
 itemRouter.post('/', authentication, authorization("admin"), itemMiddleware.createItem, itemController.createItem);
-itemRouter.patch('/:id', authentication, authorization("admin"), itemMiddleware.updateItem, itemController.updateItem);
+itemRouter.patch('/:id', authentication, itemMiddleware.updateItem, itemController.updateItem);
 itemRouter.get('/management', authentication, authorization("admin"), itemMiddleware.getItemsAdmin, itemController.getItemsAdmin);
 itemRouter.get('/', itemMiddleware.getItems, itemController.getItems);
 itemRouter.get('/promotions', itemMiddleware.getItems, itemController.getItemsWithPromotions);
